e78cf733054e2b38d35d83a77a1ea50834bffc70
akshitvigg <akshitvig48@gmail.com>
2025-08-22 23:05:22 +0530

    clap done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about , long_about=None)]
+struct Args {
+    #[arg(short, long)]
+    name: String,
+
+    #[arg(short, long, default_value_t = 1)]
+    count: u32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let arg = Args::parse();
+
+    for _ in 0..arg.count {
+        println!("Hello {}", arg.name);
+    }
 }
